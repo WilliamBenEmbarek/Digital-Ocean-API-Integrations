8778eac3c6826a0d5d40ff10cb1e1e6de77ca9e8
William Ben Embarek <william.benembarek@gmail.com>
2018-09-16 17:42:20 +0200

    Hello World!

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+keyfile = open("keys","r")
+TOKEN = keyfile.readline().rstrip()
+print(TOKEN)
+def getDroplets():
+    print("testing")    
+
+if __name__ == "__main__":
+        print("Hello")
+    
