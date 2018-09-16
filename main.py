0ba3614bb43fe8e4afcfac084a74cab0a8fd4cc9
William Ben Embarek <william.benembarek@gmail.com>
2018-09-16 17:44:16 +0200

    Testing Git

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ keyfile = open("keys","r")
 TOKEN = keyfile.readline().rstrip()
 print(TOKEN)
 def getDroplets():
-    print("testing")    
+    print("testing")
+    print("GitCraps")    
 
 if __name__ == "__main__":
         print("Hello")
